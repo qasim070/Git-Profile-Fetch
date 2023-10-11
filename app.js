df7021bdc745af188a2c95d937b31386856b66c5
qasim070 <62395180+qasim070@users.noreply.github.com>
2023-10-12 01:13:01 +0500

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
+let  data;
+let  nameDom;
+let  place;
+let  follower;
+let  following;
+let  repo;
+let  desc;
+let  git_link ;
+let  avatar ;
 let fetchGit = () =>{
     // let user = document.getElementById("").value
-    let nameDom = document.getElementById("git_name")
-    let user = document.getElementById("login")
-    fetch('https://api.github.com/users/qasim070')
+    nameDom = document.getElementById("git_name")
+    place = document.getElementById("location")
+    follower = document.getElementById("follower-count")
+    following = document.getElementById("following-count")
+    repo = document.getElementById("repo-count")
+    desc = document.getElementById("desc")
+    avatar = document.getElementById("avatar")
+    git_link = document.getElementById("link_it")
+    data = fetch('https://api.github.com/users/qasim070')
     .then((data) => data.json())
-    .then((res) => nameDom.innerHTML =  res.name )
+    .then((res) => res)
     .catch( (error) => console.error('error :' , error) )
+
 }
-fetchGit()
+
+
+let setData = () =>{
+    data.then((response) => nameDom.innerHTML = response.name );
+    data.then((response) => avatar.src = response.avatar_url );
+    data.then((response) => follower.innerHTML = response.followers );
+    data.then((response) => following.innerHTML = response.following );
+    data.then((response) => repo.innerHTML = response.public_repos );
+    data.then((response) => place.innerHTML = response.location );
+    data.then((response) => desc.innerHTML = response.bio );
+    data.then((response) => git_link.href = response.url );
+    document.getElementById("profile-card").style.opacity = "1";
+}
+setData()
+
+let check = () =>{
+    alert('hhh');
+}
